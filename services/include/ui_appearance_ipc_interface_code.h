d6d5cb51ba9ccb02610c2f3325a6e9ac79ff6c4b
liukaii <liukai240@huawei.com>
2023-06-27 20:47:15 +0800

    update CODEOENERS file.
    Signed-off-by: liukaii <liukai240@huawei.com>

--- services/include/ui_appearance_ipc_interface_code.h
+++ services/include/ui_appearance_ipc_interface_code.h
@@ -22,7 +22,6 @@ namespace ArkUi::UiAppearance {
 enum class UiAppearanceInterfaceCode {
     SET_DARK_MODE = 1,
     GET_DARK_MODE = 2,
-    ERROR,
 };
 } // namespace ArkUi::UiAppearance
 } // namespace OHOS
